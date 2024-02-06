da0f49ecb799dae10d6a56f7b4f301c11fe25522
abdurrozaqf <rozaqa27@gmail.com>
2024-02-06 19:32:36 +0700

    refactor: axiosWithConfig

--- src/libs/apis/axiosWithConfig.ts
+++ src/libs/apis/axiosWithConfig.ts
@@ -1,38 +1,14 @@
 import axios from "axios";
 
-interface Queries {
-  api_key?: string;
-  language: string;
-  session_id?: string;
-}
-
-let session_id = "";
-const axiosWithConfig = axios.create();
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-
-export const setAxiosConfig = (token: string) => {
-  session_id = token;
-};
-
-axiosWithConfig.interceptors.request.use((axiosConfig) => {
-  let queries: Queries = {
-    api_key: API_KEY,
+const axiosWithConfig = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  params: {
     language: "en-US",
-  };
-
-  if (session_id !== "") {
-    queries["session_id"] = session_id;
-  }
-
-  axiosConfig.baseURL = BASE_URL;
-  axiosConfig.params = {
-    ...axiosConfig.params,
-    ...queries,
-  };
-
-  return axiosConfig;
+  },
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_READ_ACCESS_TOKEN}`,
+  },
 });
 
 export default axiosWithConfig;
