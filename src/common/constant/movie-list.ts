d417ad8ad3cd6a6a4777f81200540af215aea325
abdurrozaqf <rozaqa27@gmail.com>
2024-04-23 15:03:41 +0700

    feat: add and update constant genre and movie list

---
+++ src/common/constant/movie-list.ts
@@ -0,0 +1,18 @@
+export const MOVIE_LIST = [
+  {
+    key: "now_playing",
+    name: "Now Playing",
+  },
+  {
+    key: "popular",
+    name: "Popular",
+  },
+  {
+    key: "top_rated",
+    name: "Top Rated",
+  },
+  {
+    key: "upcoming",
+    name: "Up Coming",
+  },
+];
