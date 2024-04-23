24bccb883be332928da25ac96d1a66a12e11cc45
abdurrozaqf <rozaqa27@gmail.com>
2024-04-23 15:02:56 +0700

    refactor: folder structure

--- src/services/providers/react-query-provider.tsx
+++ src/services/providers/react-query-provider.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
-interface RootLayoutProps {
-  children: React.ReactNode;
+interface ReactQueryProviderProps {
+  children: ReactNode;
 }
 const queryClient = new QueryClient();
-const ReactQueryProvider = ({ children }: RootLayoutProps) => {
+
+export default function ReactQueryProvider({
+  children,
+}: ReactQueryProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-};
-
-export default ReactQueryProvider;
+}
